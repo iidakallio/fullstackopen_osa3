55b923528f176282bd3f1c71ca1330e8a9ef633c
iidakallio <128421400+iidakallio@users.noreply.github.com>
2025-01-16 18:27:06 +0200

    Update index.js
    Lisätty CORS

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -110,6 +113,7 @@ app.post('/api/persons', (request, response) => {
     `)
 })
   
-  const PORT = 3001
-  app.listen(PORT)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+})
