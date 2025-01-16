9a35fa325a07843a60796a78a993ef7a87bb1b7f
iidakallio <128421400+iidakallio@users.noreply.github.com>
2025-01-16 18:44:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
-const cors = require('cors')
-
-app.use(cors())
 
 app.use(express.json())
 
@@ -113,7 +110,6 @@ app.post('/api/persons', (request, response) => {
     `)
 })
   
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+  const PORT = 3001
+  app.listen(PORT)
   console.log(`Server running on port ${PORT}`)
-})
